b3826e30e4de174a250ea8de9c52d317f2a9b390
Valentin1950 <pvipvi@i.ua>
2026-06-03 16:29:14 +0300

    My Site domstroim

---
+++ m_stp_files/ajs.php
@@ -0,0 +1,8 @@
+(function(){
+var s = "<"+"script type=\"text/javascript\" src=\"http://leader-composite.org/abnl/?adsdata=YuCc7wr1e;^w3F!jTAadQpfvwNXscq7YmAJlaNGFpfn4k2xaOMn2v7MlrA!6H3gXWYSRpyEfhQph;wt1^d;MjL5uxXZOiLYgh5hrYJHqSuIDDcJ^Xn^aGuJ0C7XTSIP3Iq70jBMAzgOaj!co\"><"+"/script>\n";
+s += "<"+"div id=\'beacon_bac5acb30b\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'>\n";
+s += "<"+"img width=\"0\" height=\"0\" src=\"http://cat.fr.eu.criteo.com/delivery/lg.php?ckmode=9&cb=bac5acb30b&loc=http%3a%2f%2fleader-composite.org%2findex%2fstekloplastik_v_stroitelstve_primery_i_osobennosti_primenenija%2f0-131&c1=oUSZAktCdY3o2H5gWcom69jaYy6x2XMRIj_uox5dkqYCZ_56rdhu8BjIVA9-oZobb05LlXNvg5n37duvPgcaJPszy-Bt0A3N1f8HSppoEzr9tzuzVRAqHrR_MtOlQ-rDZF3QYriqwrnkb8lh8Hhw0t3UTmZMq5xGFm_e1fAk_JtaMXYhDgK_NlxaFVTfwbP1&c2=VtXMEwGkGXCyvpDOxEpDylV8czh8V7HD\"/>\n";
+s += "<"+"iframe width=\"0\" height=\"0\" style=\"display:none\" src=\"http://dis.eu.criteo.com/dis/dis.aspx?pu=49205&cb=bac5acb30b\"><"+"/iframe>\n";
+s += "<"+"/div>\n";
+s += "\n";
+document.write(s);})();
